fe69147e22c79b2e5d5e4fb5fb4236ceef5acf96
jamestinio <jamestinio@gmail.com>
2020-10-27 15:19:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-vysrru
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-vysrru)
---
+++ src/app/product-alert/product-alert.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+// import { Input } from "@angular/core";
+//import { Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: "app-product-alert",
+  templateUrl: "./product-alert.component.html",
+  styleUrls: ["./product-alert.component.css"]
+})
+export class ProductAlertComponent implements OnInit {
+  @Input() product;
+  @Output() notifyBtnClick = new EventEmitter();
+  constructor() {}
+
+  ngOnInit() {}
+}
